ed21d87c170e584121b5b4aa7e2867ea6c9449fc
TDcrew <45012569+TDcrew@users.noreply.github.com>
2023-03-21 09:47:56 -0400

    Add files via upload

--- Scripts/Scraper_fcs.py
+++ Scripts/Scraper_fcs.py
@@ -7,9 +7,18 @@ def scrape_links():
     roster_links = df.iloc[:,2]
     return roster_links
 
+#function to scrape team names
+def scrape_team_names():
+    df = pd.read_excel('2023_roster_links.xlsx', sheet_name="FCS")
+    roster_names = df.iloc[:,1]
+    return roster_names
+
+# function to remove blank spaces from string
+def remove(string):
+    return string.replace(" ", "")
 
 # given all links iterate through the links and add each link to the same dataframe
-def gather_data(links):
+def gather_data(links, names):
     links_not_scrapable = []
     count = 0
     # fitler out nan values from list
@@ -17,6 +26,7 @@ def gather_data(links):
     # iterate through links and put each links data into the dataframe
     for index, link in links_filtered.iteritems():
         url_value = validators.url(link)
+        print(index)
         if url_value==True:
             create_csv = False
             # catch all links that are not scrapable by the pd.read_html() fucntion
@@ -29,20 +39,19 @@ def gather_data(links):
                 count+=1
             #issue in code with df.to_csv() function not working currently
             if create_csv:
-                link_name = str(link[9:17] + '.csv')
-                print(link_name)
+                team_name = remove(names[index])
+                link_name = str(team_name + '.csv')
                 df.to_csv(link_name)
         else:
             print("Not a URL")
     print("A total of ", count, " links not scrapable currently.")
     print("Here is a list of links that could not be scraped", links_not_scrapable)
 
-        
-
 # main function 
 def main():
     all_roster_links = scrape_links()
-    final_data = gather_data(all_roster_links)
+    all_roster_names = scrape_team_names()
+    final_data = gather_data(all_roster_links, all_roster_names)
     print(final_data)
 
 # execute main funciton
