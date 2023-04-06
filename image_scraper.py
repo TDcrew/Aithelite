651a13ac80d52daeb2be1a6d094951fa92fdf192
Isaiah Stapleton <istaplet@redhat.com>
2023-04-06 13:45:57 -0400

    Improved image logic, add output files (FCS, FBS)
    If there is table, pull names and link from table
    
    Signed-off-by: Isaiah Stapleton <stapletonin@g.cofc.edu>

--- image_scraper.py
+++ image_scraper.py
@@ -14,12 +14,14 @@ def scrape_links():
 
 all_links = scrape_links().tolist()
 
+school_name = pd.read_excel('2023_roster_links.xlsx', sheet_name="FCS").iloc[:,1]
+
 invalid_links = []
 no_image_links = []
 
 with open('player_images.csv', "w", newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Name', 'Image Source'])
+    writer.writerow(['Name', 'School', 'Image Source'])
 
     for i in range(len(all_links)):
         URL = all_links[i]
@@ -33,31 +35,51 @@ with open('player_images.csv', "w", newline='') as file:
 
         soup = BeautifulSoup(page.text, "html.parser")
 
-        images = soup.find_all('img')
+        table = soup.find('tr')
 
-        if len(images) < 50:
-            no_image_links.append(all_links[i])
+        if table:  
+            rows = soup.find_all('tr')
 
-        print(no_image_links)
+            for row in rows:
+                name_school_src = []
+                columns = row.find_all('td')
 
-        for image in images:
-            name_src = []
-            
-            # and ((image.get('alt') != None) or (image.get('alt') != "")) 
-            
-            if image.get('src') != None:
-                try: 
-                    if image.get('src')[0] == '/':
-                        name_src.append(image.get('alt'))
-                        name_src.append(base + image.get('src'))
-                    else:
-                        name_src.append(image.get('alt'))
-                        name_src.append(image.get('src'))
-                except:
-                    invalid_links.append(URL)
+                if len(columns) >= 2:
+                    name_school_src.append(columns[1].text.strip())
+                    name_school_src.append(school_name[i])
+                    link = columns[1].find('a')
+                    if link:
+                        name_school_src.append(base + link.get('href'))
+                
+                    writer.writerow(name_school_src)
 
-            
-            if name_src != []:
-                if name_src[0] != "" and name_src[0] != '' and name_src[0] != None:
-                    writer.writerow(name_src)
+
+        else:
+            images = soup.find_all('img')
+
+            if len(images) < 50:
+                no_image_links.append(all_links[i])
+
+    
+
+            for image in images:
+                name_school_src = []
+                            
+                if image.get('src') != None:
+                    try: 
+                        if image.get('src')[0] == '/':
+                            name_school_src.append(image.get('alt'))
+                            name_school_src.append(school_name[i])
+                            name_school_src.append(base + image.get('src'))
+                        else:
+                            name_school_src.append(image.get('alt'))
+                            name_school_src.append(school_name[i])
+                            name_school_src.append(image.get('src'))
+                    except:
+                        invalid_links.append(URL)
+
+                
+                if name_school_src != []:
+                    if name_school_src[0] != "" and name_school_src[0] != '' and name_school_src[0] != None:
+                        writer.writerow(name_school_src)
 
